880bac4f0efaf1ca0c9300a43435de41b290bbc5
Duncan Hoge <duncan.hoge@gmail.com>
2025-06-04 09:27:18 -0700

    Fix Card component click handling

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 interface CardProps {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-export const Card: React.FC<CardProps> = ({ children, className = '' }) => (
-  <div className={`rounded-lg border border-theme-white/20 p-6 ${className}`}>
+export const Card: React.FC<CardProps> = ({ children, className = '', onClick }) => (
+  <div 
+    className={`rounded-lg border border-theme-white/20 p-6 ${className}`}
+    onClick={onClick}
+    role={onClick ? "button" : undefined}
+    tabIndex={onClick ? 0 : undefined}
+  >
     {children}
   </div>
 );
